90e6c8ffaa8e6c80217ed33a891877033e123f41
Anders Wind <awia00@gmail.com>
2019-05-24 00:38:10 +0200

    Opengl Window

--- renderer/runner/src/main.cu
+++ renderer/runner/src/main.cu
@@ -1,9 +1,34 @@
+#include <GL/glut.h>
 #include <iostream>
 #include <path_tracer/cuda_renderer.cuh>
 #include <string>
 
-int main()
+void display_me(void)
 {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    glVertex3f(0.5, 0.0, 0.5);
+    glVertex3f(0.5, 0.0, 0.0);
+    glVertex3f(0.0, 0.5, 0.0);
+    glVertex3f(0.0, 0.0, 0.5);
+    glEnd();
+    glFlush();
+}
+
+void init_window(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello world!");
+    glutDisplayFunc(display_me);
+    glutMainLoop();
+}
+
+int main(int argc, char** argv)
+{
+    init_window(argc, argv);
     using namespace ppt::shared;
     using namespace ppt::path_tracer;
     int w = 1280;
