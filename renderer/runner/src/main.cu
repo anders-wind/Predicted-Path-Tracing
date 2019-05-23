683ca2de98ee9dc8588ad16cc63cc128a490f4ec
Anders Wind <awia00@gmail.com>
2019-05-23 13:04:04 +0200

    Fixed runner cmake so it can run now

--- renderer/runner/src/main.cu
+++ renderer/runner/src/main.cu
@@ -1,19 +1,6 @@
-#include <fstream>
 #include <iostream>
-#include <limits>
-#include <memory>
-#include <path_tracer/camera.cuh>
 #include <path_tracer/cuda_renderer.cuh>
-#include <path_tracer/hitable.cuh>
-#include <path_tracer/hitable_list.cuh>
-#include <path_tracer/material.cuh>
-#include <path_tracer/ray.cuh>
-#include <path_tracer/sphere.cuh>
-#include <random>
-#include <shared/random_helpers.cuh>
-#include <shared/vec3.cuh>
 #include <string>
-#include <vector>
 
 int main()
 {
@@ -24,15 +11,8 @@ int main()
     int s = 400;
     try
     {
-        std::cout << "A" << std::endl;
-
         auto colors = cuda_renderer::cuda_ray_render(w, h, s);
-
-        std::cout << "B" << std::endl;
-
         cuda_renderer::write_ppm_image(colors, w, h, "render");
-
-        std::cout << "C" << std::endl;
     }
     catch (...)
     {
