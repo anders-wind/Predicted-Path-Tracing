cb0516cd94fae217e62e53d38deae62d045db849
Anders Wind <awia00@gmail.com>
2019-06-04 19:33:55 +0200

    Better show images

--- predicter/run.py
+++ predicter/run.py
@@ -42,13 +42,26 @@ def show_images(net, input_render, target_image):
     Plots the input_render, the target_image, and the prediction image
     """
     predicted = net.forward_single(input_render)
-    figure, axarr = plt.subplots(ncols=3)
-    axarr[0].imshow(input_render.detach().cpu().numpy().transpose((1, 2, 0))[:, :, :3], label="input")
-    axarr[0].title.set_text('input')
-    axarr[1].imshow(target_image.detach().cpu().numpy().transpose((1, 2, 0)), label="target")
-    axarr[1].title.set_text('target')
-    axarr[2].imshow(predicted.detach().cpu().numpy().transpose((1, 2, 0)), label="prediction")
-    axarr[2].title.set_text('prediction')
+    figure, axarr = plt.subplots(nrows=2, ncols=3)
+    input_render = input_render.detach().cpu().numpy().transpose((1, 2, 0))[:, :, :3]
+    target_image = target_image.detach().cpu().numpy().transpose((1, 2, 0))
+    predicted = predicted.detach().cpu().numpy().transpose((1, 2, 0))
+    diff_origin = np.sqrt(np.square(target_image - input_render))
+    diff_input = np.sqrt(np.square(predicted - input_render))
+    diff_target = np.sqrt(np.square(predicted - target_image))
+    axarr[0, 0].imshow(input_render, label="input")
+    axarr[0, 0].title.set_text('input')
+    axarr[0, 1].imshow(target_image, label="target")
+    axarr[0, 1].title.set_text('target')
+    axarr[0, 2].imshow(predicted, label="prediction")
+    axarr[0, 2].title.set_text('prediction')
+
+    axarr[1, 0].imshow(diff_origin, label="target - input")
+    axarr[1, 0].title.set_text('target - input')
+    axarr[1, 1].imshow(diff_input, label="predict - input")
+    axarr[1, 1].title.set_text('predict - input')
+    axarr[1, 2].imshow(diff_target, label="predict - target")
+    axarr[1, 2].title.set_text('predict - target')
     figure.show()
 
 
