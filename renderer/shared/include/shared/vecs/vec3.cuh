616219939cb8eca097ef5a8b1311b7aaa61e5cf6
Anders Wind <awia00@gmail.com>
2019-06-17 13:23:49 +0200

    Fixed cross product

--- renderer/shared/include/shared/vecs/vec3.cuh
+++ renderer/shared/include/shared/vecs/vec3.cuh
@@ -198,7 +198,7 @@ __host__ __device__ inline vec3 operator/(const vec3& v1, const vec3& v2)
 
 __host__ __device__ inline vec3 cross(const vec3& v1, const vec3& v2)
 {
-    return vec3(v1[1] * v2[2] - v1[2] * v2[1], v1[0] * v2[2] - v1[2] * v2[0], v1[0] * v2[1] - v1[1] * v2[0]);
+    return vec3(v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2], v1[0] * v2[1] - v1[1] * v2[0]);
 }
 
 __host__ __device__ inline float dot(const vec3& v1, const vec3& v2)
